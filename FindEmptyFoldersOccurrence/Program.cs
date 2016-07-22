2e806eca038ad3ced4e58bcfe43c23f984f5f208
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2016-07-22 17:46:39 +1000

    Added method to get a list of all directories.

--- FindEmptyFoldersOccurrence/Program.cs
+++ FindEmptyFoldersOccurrence/Program.cs
@@ -11,7 +11,27 @@ namespace FindEmptyFoldersOccurrence
     {
         static void Main(string[] args)
         {
-            
+            var directoryList = GetDirectoryList(@"P:\");
+        }
+
+        private static object GetDirectoryList(string searchPath)
+        {
+            try
+            {
+                var files = from directory in Directory.EnumerateDirectories(searchPath, "*.*", SearchOption.AllDirectories)
+                            select new
+                            {
+                                Directory = directory
+                            };
+
+                return files;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
         }
 
         private static bool IsDirectoryEmpty(string path)
